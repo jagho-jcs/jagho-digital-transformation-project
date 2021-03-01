713b665e1bc1f0b5e318d9074be7ee02dfe3ce2b
Yomi Ogunyinka <kbab6aoo@hotmail.com>
2021-03-01 01:35:50 +0000

    file updated

--- terraform/modules/jcs-gitlab-repo/main.tf
+++ terraform/modules/jcs-gitlab-repo/main.tf
@@ -2,5 +2,6 @@ resource "gitlab_project" "this" {
   name              = var.name
   description       = var.description
 
-  visibility_level  = var.visibility_level
+  visibility_level  = var.visibility_level  // can be private, internal, public
+  default_branch    = var.default_branch
 }
